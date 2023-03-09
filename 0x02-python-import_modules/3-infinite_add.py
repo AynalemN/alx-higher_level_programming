0afc26a6329ad8ef7239ef2fee9df7c7e203a1c5
AynalemN <abebeayni@gmail.com>
2023-03-09 06:43:54 -0800

    all all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    argc = len(argv)
+    n = len(argv)
     sum = 0
-if argc == 0:
+if n == 0:
     print(f'{int(argc)}')
-for i in range(1, argc):
+for i in range(1, n):
     sum += int(argv[i])
 print(f'{sum: d}')
