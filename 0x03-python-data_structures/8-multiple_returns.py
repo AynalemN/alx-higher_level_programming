1408f402e4743471ade6736325957c9470deefaa
AynalemN <abebeayni@gmail.com>
2023-03-12 03:46:40 -0700

    minor fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,8 @@ def multiple_returns(sentence):
     leng = len(sentence)
     first_char = sentence[0]
     if leng > 0:
-        return (leng, first_char)
+        r = leng, first_char
+        return r
     else:
-        return None
+        r = leng, None
+        return r
