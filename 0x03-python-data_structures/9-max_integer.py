b35741878dabffb0417f2ea2cd49b6748c656953
AynalemN <abebeayni@gmail.com>
2023-03-12 04:07:22 -0700

    minor fix

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,5 +6,4 @@ def max_integer(my_list=[]):
         for idx in range(0, len(my_list)):
             if my_list[idx] < my_list[idx + 1]:
                 return my_list[idx + 1]
-            else:
-                return my_list[idx]
+        return my_list[idx]
