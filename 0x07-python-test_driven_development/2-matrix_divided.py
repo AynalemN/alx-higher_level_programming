dd843ce80005210db5df8c5d90734013f7ffe0da
AynalemN <abebeayni@gmail.com>
2023-04-06 05:42:59 -0700

    syntax is editted

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,16 +11,16 @@ def matrix_divided(matrix, div):
     """
     listError = 'matrix must be a matrix (list of lists) of integers/floats'
     sizeError = 'Each row of the matrix must have the same size'
-    if type(matrix) is not list:
+    if type(matrix) != list:
         raise TypeError(listError)
     for item in range(len(matrix)):
-        if item is not 0:
+        if item != 0:
             result = item - 1
-            if len(matrix[item]) is not len(matrix[result]):
+            if len(matrix[item]) != len(matrix[result]):
                 raise TypeError(sizeError)
     if isinstance(div, int) is False:
         raise TypeError('div must be a number')
-    if div is 0:
+    if div == 0:
         raise ZeroDivisionError('division by zero')
 
     return [[round(item / div, 2) for item in m_list] for m_list in matrix]
