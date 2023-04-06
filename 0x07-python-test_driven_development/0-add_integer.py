8cbae39413fb2938bfb1ec4a5eb2a18ac3a3d36f
AynalemN <abebeayni@gmail.com>
2023-04-06 05:24:40 -0700

    add integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" Addition """
+
+def add_integer(a, b=98):
+    """ adding two numbers """
+    if isinstance(a, (int, float)) is False:
+        raise TypeError('a must be an integer')
+    if isinstance(b, (int, float)) is False:
+        raise TypeError('b must be an integer')
+    else:
+        return int(a) + int(b)
